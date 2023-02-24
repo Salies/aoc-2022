1d47ad8e41ce8420b282ff2959026b175e6fa7ac
Salies <serezane@protonmail.com>
2023-02-23 22:41:43 -0300

    day 7

--- aoc/day7.py
+++ aoc/day7.py
@@ -15,6 +15,29 @@ class File(Node):
 @dataclass
 class Directory(Node):
     children: dict
+    size: int = 0
+
+# calculating sizes in post-order traversal, recursively
+# I'm using post-order from the root here because I'm not sure if the last input is a leaf or not
+total_sum = 0
+def calculate_size(node):
+    if isinstance(node, File):
+        return node.size
+    for child in node.children.values():
+        node.size += calculate_size(child)
+    if node.size <= 100000:
+        global total_sum
+        total_sum += node.size
+    return node.size
+
+def get_dir_to_delete(dir):
+    global root_dir
+    global curr_min
+    if root_dir.size - dir.size < 40000000 and dir.size < curr_min:
+        curr_min = dir.size
+    for child in dir.children.values():
+        if isinstance(child, Directory):
+            get_dir_to_delete(child)
 
 # now let's construct the actual tree from the input
 f = open("inputs/day7.txt", "r")
@@ -28,9 +51,9 @@ lines.pop(0)
 root_dir = Directory("/", None, {})
 curr_dir = root_dir
 for line in lines:
-    # ls can be ignored
-    # it's possible to use just line[2:4] == "ls" (or, rahter, line[2] == "l"), but I'm leaving the redundant check in
     if line[0] == "$":
+        # ls can be ignored
+        # it's possible to use just line[2:4] == "ls" (or, rahter, line[2] == "l"), but I'm leaving the redundant check in
         if line[2:4] == "ls":
             continue
         _, directory = line[2:].strip().split(' ')
@@ -57,3 +80,10 @@ for line in lines:
     fp = int(fp) # fp is the filesize
     if sp not in curr_dir.children:
         curr_dir.children[sp] = File(sp, curr_dir, fp)
+
+calculate_size(root_dir)
+print("Part one:", total_sum)
+
+curr_min = root_dir.size
+get_dir_to_delete(root_dir)
+print("Part two:", curr_min)
